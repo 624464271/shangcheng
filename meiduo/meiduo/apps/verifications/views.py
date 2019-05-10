52db278bcf4a7af6b09c7ea78dbd4b43313b33c7
huozhanping <624464271@qq.com>
2019-05-11 00:50:16 +0800

    初始化

---
+++ meiduo/meiduo/apps/verifications/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from meiduo.libs.captcha.captcha import captcha
+from django_redis import get_redis_connection
+from . import constants
+# from . import constants
+
+# url('^image_codes/(?P<image_code_id>[\w-]+)/$', views.ImageCodeView.as_view()),
+class ImageCodeView(APIView):
+    """
+    图片验证码
+    """
+    pass
+
+
+class ImageCodeView(APIView):
+    """
+    图片验证码
+    """
+
+    def get(self, request, image_code_id):
+        """
+        获取图片验证码
+        """
+        # 生成验证码图片
+        text, image = captcha.generate_captcha()
+        captcha
+
+
+        redis_conn = get_redis_connection("verify")
+        redis_conn.setex("img_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
+        # redis_conn.setex("img_%s" % image_code_id, 300,text)
+
+        # 固定返回验证码图片数据，不需要REST framework框架的Response帮助我们决定返回响应数据的格式
+        # 所以此处直接使用Django原生的HttpResponse即可
+        return HttpResponse(image, content_type="images/jpg")
